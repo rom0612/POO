bd55f2903f91aab4e0078e5fc30d3f5c219404b2
rom <roman.ramos.322@gmail.com>
2025-08-22 08:43:34 -0600

    Change NameClass to Practica1_2

--- Practica1_2.java
+++ Practica1_2.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Menu {
+public class Practica1_2 {
     
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -19,7 +19,7 @@ public class Menu {
             
             switch (opcion) {
                 case 1:
-                    System.out.println("Ingres el número para calcular su factorial: ");
+                    System.out.println("Ingrese el número para calcular su factorial: ");
                     int numFactorial = scanner.nextInt();
                     if (numFactorial < 0) {
                         System.out.println("Error: No puede calcularse para numeros negativos");
@@ -32,16 +32,22 @@ public class Menu {
                 case 2:
                     System.out.print("Ingrese la posición en la secuencia de Fibonacci: ");
                     int numFibonacci = scanner.nextInt();
-                    if (numFibonacci < 0) {
+                    
+                    if (numFibonacci < 0) { // Control de excepciones:
                         System.out.println("Error: La posición debe ser un número positivo");
                     } else {
-                        long resultadoFibonacci = fibonacci(numFibonacci);
-                        System.out.println("El término en la posición " + numFibonacci + " de Fibonacci es: " + resultadoFibonacci);
+                        //Implementación de for para mostrar secuencia completa
+                        for(int i=0;i<numFibonacci;i++){
+                            System.out.print(fibonacci(i)+ " ");
+                        }
                     }
                     break;
                     
                 case 3:
-                    System.out.print("Se selecc. Collatz");
+                    System.out.print("Ingrese un numero para secuencia de Collatz: ");
+                    int numCollatz = scanner.nextInt();
+                    collatz(numCollatz);
+                    System.out.println();
                     break;
                     
                 case 4:
@@ -58,18 +64,33 @@ public class Menu {
     }
 
     public static long factorial(int n) {
-        if (n == 0 || n == 1) {
+        if (n == 0 || n == 1) { // Casos base 
             return 1;
         }
-        return n * factorial(n - 1);
+        return n * factorial(n - 1); // aplica recursividad con n-1
     }
 
     public static long fibonacci(int n) {
+        // Casos base 0 y 1
         if (n == 0) {
             return 0;
-        } else if (n == 1) {
-            return 1;
+        } else if (n == 1) { 
+            return 1; 
         }
-        return fibonacci(n - 1) + fibonacci(n - 2);
+        //caso promedio
+        return fibonacci(n - 1) + fibonacci(n - 2); // aplicando recursividad con las reglas de fibonacci
+        }
+    public static void collatz(int n){
+        System.out.print(n + " ");
+    if (n == 1) { // Caso base
+            return; 
+        } else if (n%2==0) { // si es par, se divide entre 2 
+            collatz(n/2);
+        } else { // si es impar, se mult. por 3 y se le suma 1
+            collatz(3*n+1);
+        }
+        
+        }
     }
-}
+
+
